5091b81e16ff9cb03090b1f4085b5957ba3d102f
david barkhuizen <david.barkhuizen@gmail.com>
2017-04-24 16:10:47 +0200

    refactor - code to generate list of symbols with filepaths extracted

--- clean_csv_data.py
+++ clean_csv_data.py
@@ -11,92 +11,75 @@ from csvhandler import CSV
 from httpadaptor import *
 
 ONE_DAY = datetime.timedelta(days=1) 
-
 DELAY = 0.25
-CSV_ROOT_PATH = '/home/david/data/csv_data/'
 
 # -------------------------------------------------------------
 
 def clean_csv_data_file_tree(root_folder):
-	""" """  
 
-	# load csv list of symbol types from file 'symbol_types.csv' in root folder
-	symbol_types_list_file_path = root_folder + 'symbol_types' + '.csv'
-	log('loading csv list of symbol types @ %s' % symbol_types_list_file_path)
-	try:
-		symbol_types = CSV.load_csv_list_from_file(symbol_types_list_file_path)
-		log('ok')
-	except Exception, e:
-		log('exception - %s' % str(e))
-		return
+	symbol_filepaths = get_symbols_with_filepath(root_folder)
+	random.shuffle(symbol_filepaths)
+
+	count = 0
+	for (symbol, symbol_file_path) in symbol_filepaths:
+	
+		log(symbol)
+	
+		count += 1     
+		timer_start = time.clock()      
+
+  		csv_file_exists = CSV.file_exists(symbol_file_path)
   
-	for symbol_type in symbol_types:
+  		if not csv_file_exists:
+			log('no csv file found')
+			continue
+
+		log('csv file %s exists' % symbol_file_path)
 		
-		folder = root_folder + symbol_type + '/'
-		symbol_list_file_path = folder + 'symbols' + '.csv'
-		log('loading csv list of symbols @ %s' % symbol_list_file_path)
-		symbols = CSV.load_csv_list_from_file(symbol_list_file_path)
-		log('ok')
-	
-		count = 0
-		for symbol in symbols:
-			
-			log(symbol)
-			
-			count += 1     
-			timer_start = time.clock()      
-	  
-	  		symbol_file_path = folder + symbol + '.csv'
-	  		csv_file_exists = CSV.file_exists(symbol_file_path)
-	  
-	  		if not csv_file_exists:
-				log('no csv file found')
-				continue
+		text = ''
+		with open(symbol_file_path, 'rt') as csv_file:
+			text = csv_file.read()
+		
+		raw_lines = text.split('\n')
 
-			log('csv file %s exists' % symbol_file_path)
-			
-			text = ''
-			with open(symbol_file_path, 'rt') as csv_file:
-				text = csv_file.read()
-			
-			raw_lines = text.split('\n')
+		cleaned_lines = []
 
-			cleaned_lines = []
+		for raw_line in raw_lines:
 
-			for raw_line in raw_lines:
+			data = raw_line.split(',')
 
-				data = raw_line.split(',')
+			if len(data) != 13:
+				if len(data) > 1:
+					cleaned_lines.append(raw_line)
+			else:
+				left = data[:6]
+				middle = data[6]
+				right = data[7:]
 
-				if len(data) != 13:
-					if len(data) > 1:
-						cleaned_lines.append(raw_line)
-				else:
-					left = data[:6]
-					middle = data[6]
-					right = data[7:]
+				middle_right = middle[-10:]
+				middle_left = middle[:-10]
 
-					middle_right = middle[-10:]
-					middle_left = middle[:-10]
+				left.append(middle_left)
+				temp = [middle_right]
+				temp.extend(right)
+				right = temp
 
-					left.append(middle_left)
-					temp = [middle_right]
-					temp.extend(right)
-					right = temp
+				cleaned_lines.append(','.join(left).replace('\r', ''))
+				cleaned_lines.append(','.join(right).replace('\r', ''))
 
-					cleaned_lines.append(','.join(left).replace('\r', ''))
-					cleaned_lines.append(','.join(right).replace('\r', ''))
+		out_text = '\n'.join(cleaned_lines)
 
-			out_text = '\n'.join(cleaned_lines)
-	
-			file_access_mode = 'wt' # 'at' if csv_file_exists else 'wt'
-			with open(symbol_file_path, file_access_mode) as out_file:
-				out_file.write(out_text)
-			
-			print(symbol_file_path)
+		file_access_mode = 'wt' # 'at' if csv_file_exists else 'wt'
+		with open(symbol_file_path, file_access_mode) as out_file:
+			out_file.write(out_text)
+		
+		print(symbol_file_path)
   
 def main():
 	log('root path = %s' % CSV_ROOT_PATH)
-	clean_csv_data_file_tree(CSV_ROOT_PATH)
+	#clean_csv_data_file_tree(CSV_ROOT_PATH)
+
+	print(get_symbols_with_filepath(CSV_ROOT_PATH))
   
 if __name__ == '__main__':
 	main()
--- yfh.py
+++ yfh.py
@@ -1,3 +1,5 @@
+from pycore import configure_logging, log
+configure_logging('csv_data_update.log')
 
 import time
 import datetime
@@ -8,8 +10,6 @@ import random
 from csvhandler import CSV
 from httpadaptor import *
 
-from pycore import configure_logging, log
-
 ONE_DAY = datetime.timedelta(days=1) 
 
 WAIT_BETWEEN_SYMBOLS_S = 1
@@ -17,6 +17,28 @@ CSV_ROOT_PATH = '/home/mage/data/csv_data_dec_2015/'
 
 NETWORK_RETRY_LIMIT = 1000
 NETWORK_RETRY_WAIT_S = 10 
+
+# -------------------------------------------------------------
+
+def get_symbols_with_filepath(root_folder):
+
+	symbol_filepath = []
+
+	symbol_types_list_file_path = root_folder + 'symbol_types' + '.csv'
+	symbol_types = CSV.load_csv_list_from_file(symbol_types_list_file_path)
+  
+	for symbol_type in symbol_types:
+		
+		folder = root_folder + symbol_type + '/'
+		symbol_list_file_path = folder + 'symbols' + '.csv'
+		symbols = CSV.load_csv_list_from_file(symbol_list_file_path)
+	
+		for symbol in symbols:
+	  		symbol_file_path = folder + symbol + '.csv'
+
+	  		symbol_filepath.append((symbol, symbol_file_path))
+
+	return symbol_filepath
 
 # -------------------------------------------------------------
 
@@ -30,129 +52,109 @@ def update_csv_to_present_from_net(root_folder):
 
 	'''  
 
-	# load csv list of symbol types from file 'symbol_types.csv' in root folder
-	symbol_types_list_file_path = root_folder + 'symbol_types' + '.csv'
-	log('loading csv list of symbol types @ %s' % symbol_types_list_file_path)
-	try:
-		symbol_types = CSV.load_csv_list_from_file(symbol_types_list_file_path)
-		random.shuffle(symbol_types)
-		log('ok')
-	except Exception, e:
-		log('exception - %s' % str(e))
-		return
+	symbol_filepaths = get_symbols_with_filepath(root_folder)
+	random.shuffle(symbol_filepaths)
+
+	count = 0
+	for symbol, symbol_file_path in symbol_filepaths:
+		
+		log('-'*80)
+		log('{0} - {1}'.format(symbol, symbol_file_path))
+		
+		count += 1     
+		timer_start = time.clock()      
   
-	for symbol_type in symbol_types:
-		
-		folder = root_folder + symbol_type + '/'
-		symbol_list_file_path = folder + 'symbols' + '.csv'
-		log('loading csv list of symbols @ %s' % symbol_list_file_path)
-		symbols = CSV.load_csv_list_from_file(symbol_list_file_path)
-		random.shuffle(symbols)
-		log('ok')
-	
-		count = 0
-		for symbol in symbols:
-			
-			log('-'*80)
-			log(symbol)
-			
-			count += 1     
-			timer_start = time.clock()      
-	  
-	  		symbol_file_path = folder + symbol + '.csv'
-	  		csv_file_exists = CSV.file_exists(symbol_file_path)
-	  
-	  		if (csv_file_exists == True):
-				log('csv file %s exists' % symbol_file_path)
-				try:
-					(d_start, d_end) = CSV.get_date_range(symbol_file_path)
-				except Exception, e:
-					log('exception encountered while examining existing data file, skipping symbol')
-					continue
-
-				log('existing data spans range %s - %s' % (str(d_start), str(d_end)))
-				i = d_end + ONE_DAY        
-	  		else:
-				log('no csv file found')
-				i = datetime.date(year=1900,month=1,day=1)    
-		
-	  		f = datetime.datetime.now()
-
-	  		if (i.year == f.year) and (i.month == f.month) and (i.day == f.day):
-				log('stored data is current')
-				continue # to next symbol
- 
-			body = None
-
-			msg = 'requesting data from {0}-{1}-{2} to {3}-{4}-{5}'.format(
-				str(i.year).zfill(2), str(i.month).zfill(2), str(i.day).zfill(2),
-				str(f.year).zfill(2), str(f.month).zfill(2), str(f.day).zfill(2)
-				) 
-			log(msg)
-
-			retry_count = 0
-
-			while retry_count < NETWORK_RETRY_LIMIT:
-
-				try:
-
-					body = get_csv_lines_for_period(symbol,
-						str(i.year).zfill(2), str(i.month).zfill(2), str(i.day).zfill(2),
-						str(f.year).zfill(2), str(f.month).zfill(2), str(f.day).zfill(2)
-						)
-
-					break
-
-				except Exception, e:
-
-					retry_count = retry_count + 1
-					log('attempt %i to fetch data failed: %s' % (retry_count, str(e)))
-
-					if retry_count < NETWORK_RETRY_LIMIT:
-						log('sleeping for %i seconds before retrying')
-						time.sleep(NETWORK_RETRY_WAIT_S)
-	
-			data_lines = body.split('\n')[1:-1]
-			log('# of data lines returned = %s' % str(len(data_lines)))
-	
-			to_sort = []
-	
-			for line in data_lines:
-	
-				row = line.split(',')
-			 	d = CSV.row_to_dict(row)
-
-			  	if d is None:
-			  		continue
-	  
-			  	is_valid = d['date'] > d_end if csv_file_exists else True 
-			  
-				if not is_valid:
-					continue
-					
-				datum = {
-					'line' : line,
-					'date' : d['date']
-					}
-				to_sort.append(datum)
-			
-			seq = sorted(to_sort, key=lambda j:j.get('date'))	  
-		
-			file_access_mode = 'at' if csv_file_exists else 'wt'
-			new_text = '\n'.join([x['line'] for x in seq])
-			with open(symbol_file_path, file_access_mode) as out_file:
-				out_file.write(new_text)
-			
-			timer_end = time.clock()
-			
-			log('%s : %s [%i / %i] in %i seconds' % (symbol_type, symbol, count, len(symbols), timer_end - timer_start))
-			
-			log('sleeping for %i seconds before next symbol' % WAIT_BETWEEN_SYMBOLS_S)
-			time.sleep(WAIT_BETWEEN_SYMBOLS_S) 
+  		csv_file_exists = CSV.file_exists(symbol_file_path)
   
-def main():
+  		if (csv_file_exists == True):
+			log('csv file %s exists' % symbol_file_path)
+			try:
+				(d_start, d_end) = CSV.get_date_range(symbol_file_path)
+			except Exception, e:
+				log('exception encountered while examining existing data file, skipping symbol')
+				continue
 
-	configure_logging('csv_data_update.log')
+			log('existing data spans range %s - %s' % (str(d_start), str(d_end)))
+			i = d_end + ONE_DAY        
+  		else:
+			log('no csv file found')
+			i = datetime.date(year=1900,month=1,day=1)    
+	
+  		f = datetime.datetime.now()
+
+  		if (i.year == f.year) and (i.month == f.month) and (i.day == f.day):
+			log('stored data is current')
+			continue # to next symbol
+
+		body = None
+
+		msg = 'requesting data from {0}-{1}-{2} to {3}-{4}-{5}'.format(
+			str(i.year).zfill(2), str(i.month).zfill(2), str(i.day).zfill(2),
+			str(f.year).zfill(2), str(f.month).zfill(2), str(f.day).zfill(2)
+			) 
+		log(msg)
+
+		retry_count = 0
+
+		while retry_count < NETWORK_RETRY_LIMIT:
+
+			try:
+
+				body = get_csv_lines_for_period(symbol,
+					str(i.year).zfill(2), str(i.month).zfill(2), str(i.day).zfill(2),
+					str(f.year).zfill(2), str(f.month).zfill(2), str(f.day).zfill(2)
+					)
+
+				break
+
+			except Exception, e:
+
+				retry_count = retry_count + 1
+				log('attempt %i to fetch data failed: %s' % (retry_count, str(e)))
+
+				if retry_count < NETWORK_RETRY_LIMIT:
+					log('sleeping for %i seconds before retrying')
+					time.sleep(NETWORK_RETRY_WAIT_S)
+
+		data_lines = body.split('\n')[1:-1]
+		log('# of data lines returned = %s' % str(len(data_lines)))
+
+		to_sort = []
+
+		for line in data_lines:
+
+			row = line.split(',')
+		 	d = CSV.row_to_dict(row)
+
+		  	if d is None:
+		  		continue
+  
+		  	is_valid = d['date'] > d_end if csv_file_exists else True 
+		  
+			if not is_valid:
+				continue
+				
+			datum = {
+				'line' : line,
+				'date' : d['date']
+				}
+			to_sort.append(datum)
+		
+		seq = sorted(to_sort, key=lambda j:j.get('date'))	  
+	
+		file_access_mode = 'at' if csv_file_exists else 'wt'
+		new_text = '\n'.join([x['line'] for x in seq])
+		with open(symbol_file_path, file_access_mode) as out_file:
+			out_file.write(new_text)
+		
+		timer_end = time.clock()
+		
+		log('{0} [{1} / {2}] in {3} seconds'.format(symbol, count, len(symbol_filepaths), timer_end - timer_start))
+		
+		log('sleeping for %i seconds before next symbol' % WAIT_BETWEEN_SYMBOLS_S)
+		time.sleep(WAIT_BETWEEN_SYMBOLS_S) 
+  
+def main():	
 
 	log('root path = %s' % CSV_ROOT_PATH)
 	update_csv_to_present_from_net(CSV_ROOT_PATH)
